f82f08d995b9a106f52974572742c7ccb4a888f5
Inkyy <116722736+InkyyPinkyy@users.noreply.github.com>
2025-04-29 17:31:12 +0200

    Added the first two main files

---
+++ __main__.py
@@ -0,0 +1,184 @@
+from classes import *
+
+def enemy_encounter(player, enemy):
+    '''
+    initialisiert einen Kampf zwischen dem Spieler 
+    und einem Gegner. Am Anfang gibt es die Option,
+    zu fliehen oder den Kampf zu starten.
+    Beim fliehen wird der Kampf beendet, beim Kämpfen
+    wird er richtig initialisiert. 
+    Dabei werden der Schaden des Spielers und der
+    des Gegners verschieden ausgerechnet.
+    '''
+    print(f"\nEin {enemy.name} greift an!")
+    
+    while player.hp > 0 and enemy.hp > 0:
+        print(f"\n{player.name}: {player.hp}/{player.max_max_hp} HP")
+        print(f"{enemy.name}: {enemy.hp}/{enemy.max_hp} HP")
+
+        action = input("Angreifen (a) oder Fliehen (f)? ")
+        if action.lower() == 'a':
+            try: damage = random.uniform(
+                ((player.attack_damage + player.weapon.weapon_damage) * player.strength) * 0.9,
+                ((player.attack_damage + player.weapon.weapon_damage) * player.strength) * 1.1
+            )
+            except: damage = random.uniform(
+                ((player.attack_damage) * player.strength) * 0.9,
+                ((player.attack_damage) * player.strength) * 1.1
+            )
+            enemy.hp -= round(damage)
+            # (f"{damage:.2f})
+            print(f"Du fügst dem {enemy.name} {round(damage)} Schaden zu!")
+
+            if enemy.hp > 0:
+                damage = random.uniform(
+                enemy.attack_damage * player.strength * 0.9,
+                enemy.attack_damage * player.strength * 1.1
+            )
+                player.hp -= round(damage)
+                print(f"Der {enemy.name} fügt dir {round(damage)} Schaden zu!")
+
+        elif action.lower() == 'f':
+            print("Du bist geflohen!")
+            return
+
+    if player.hp > 0:
+        print(f"Du hast den {enemy.name} besiegt!")
+        gold_earned = random.randint(10, 20)
+        player.gold += gold_earned
+        print(f"Du erhältst {gold_earned} Gold!")
+        xp_earned = random.randint(enemy.max_hp // 10, enemy.max_hp // 3)
+        player.xp += xp_earned
+        print(f"Du erhältst {xp_earned} XP!")
+        check_amount_player_xp(player)
+        
+    else:
+        print("Du wurdest besiegt...")
+        exit()
+
+def check_amount_player_xp(player):
+    while player.xp >= (player.level * 32):
+        player.xp -= (player.level * 32)
+        player.level += 1
+        player.skillpoints += 1
+        print(f"Du hast Level {player.level} erreicht und einen Skillpunkt bekommen!")
+
+def show_player_stats(player):
+        print(f"\n{player.name}'s Stats:")
+        print(f"Rasse: {player.name_of_player_class}")
+        print(f"{round(player.hp)}/{round(player.max_max_hp)} HP")
+        print(f"Damage:", (player.attack_damage + player.weapon.weapon_damage) * player.strength)
+        print(f"Deine Waffe: {player.weapon}")
+        print(f"Deine Rüstung: {player.armor}")
+        print(f"Dein Ring: {player.ring}")
+        print(f"Deine Kette: {player.necklace}")
+        print(f"Dein Level: {player.level}")
+        print(f"{player.xp}/{player.level * 32} XP bis zum nächsten Level")
+        print(f"{player.skillpoints} Skillpoints")
+        print(f"Gold: {player.gold}")
+
+def event_enemy_encounter(player):
+    monster_data = get_random_monster(monsters)
+    gegner = Enemy(monster_data)
+    enemy_encounter(player, gegner)
+
+def choose_equipment(player):
+        print(f"Wähle einen Gegenstand zum Ausrüsten:")
+        player.show_inventory()
+
+        try:
+            wahl = int(input(f"\nGib die Nummer des Gegenstandes ein, den du auwählen möchtest: ")) -1
+            if 0 <= wahl < len(player.inventory):
+                item = player.inventory[wahl]
+                if item.type == "weapon":
+                    player.weapon = item
+                    print(f"{player.name} hat '{item.name}' als Waffe ausgerüstet!")
+                elif item.type == "armor":
+                    player.armor = item
+                    print(f"{player.name} hat '{item.name}' als Rüstung ausgerüstet!")
+                elif item.type == "ring":
+                    player.ring = item
+                    print(f"{player.name} hat '{item.name}' als Ring ausgerüstet!")
+                else:
+                    print(f"Dieses Item kann nicht ausgerüstet werden!")
+            else:
+                print(f"ungültige Auswahl")
+        except ValueError:
+            print(f"Bitte gib eine gültige Zahl ein")
+
+def do_inventory_shit(player):
+    print("\nInventar:")
+    if player.inventory:
+        for idx, item in enumerate(player.inventory, start = 1):
+            print(f"{idx}. {item}")
+    else:
+        print("Dein Inventar ist leer")
+    action = input("\nGegenstand ausrüsten (1), Gegenstand konsumieren (2) oder zurückkehren (3)? ")
+    if action.lower() == '1':
+        choose_equipment(player)
+    elif action.lower() == '2':
+        pass
+    elif action.lower() == '3':
+        return
+
+def do_random_event(events):
+    random_event = random.choice(list(events))()
+    return random_event
+    
+def welcome_player():
+    print(f"\nWillkommen zu deinem neuen Abenteuer!")
+    print(f"Du wachst an einem dir unbekannten Strand auf und erinnerst dich an nichts...\nNicht einmal dein eigener Name fällt dir ein...")
+    print(f"In deinen Taschen findest du ein Messer, es ist nicht viel,\naber du solltest dich im Notfall verteidigen können.")
+    name = input("Also, wie möchtest du in deinem neuen Leben heißen?\nNeuer Name: ")
+    return name
+
+def choose_class():
+    print(f"\nWähle eine Klasse:")
+    for i, c in enumerate(CLASSES.keys(), 1):
+        print(f"{i}. {c}")
+    choice = int(input(f"Deine Wahl: "))
+    class_name = list(CLASSES.keys())[choice - 1]
+    return class_name
+
+def main():
+    if input(f"Neues Spiel (n) oder Laden (l)?\n").lower() == 'l':
+        name = input("Gib deinen Spielernamen ein: ")
+        player = Player.load_game(f"{name}_save.json")
+        if not player:
+            return
+    else:
+        name = welcome_player()
+        class_name = choose_class()
+        player = Player(name, CLASSES[class_name])
+        Messer = weapon("Messer", "weapon","Ein kleines Messer, dass du in deiner Tasche gefunden hast", "messer", 5)
+        Drachenschwert = weapon("Drachenschwert", "weapon", "Random-ass Schwert zu test-zwecken", "Schwert", 6969)
+        RandomAssRing = ring("Ring der Rache oder so", "ring", "Random ass test-ring", "rache", 69)
+        player.weapon = Messer
+        player.inventory.append(Messer)
+        player.inventory.append(Drachenschwert)
+        player.inventory.append(RandomAssRing)
+        player.hp = player.max_max_hp
+
+    while True:
+        print(f"\nWas möchtest du tun?")
+        print(f"1. Erkunden")
+        print(f"2. Inventar ansehen")
+        print(f"3. Spiel speichern")
+        print(f"4. Beenden")
+        print(f"5. Deine Stats ansehen")
+        choice = input("Deine Wahl: ")
+
+        if choice == '1':
+            event_enemy_encounter(player)
+        elif choice == '2':
+            do_inventory_shit(player)
+        elif choice == '3':
+            player.save_game()
+            print("Spiel gespeichert!")
+        elif choice == '4':
+            print("Spiel beendet!")
+            break
+        elif choice == '5':
+            show_player_stats(player)
+
+main()
---
+++ classes.py
@@ -0,0 +1,244 @@
+import random
+import os
+import json
+
+class Player:
+    def __init__(self, name,player_class):
+        self.name = name
+        self.player_class = player_class
+        self.name_of_player_class = player_class['name_of_player_class']
+        
+        self.skillpoints = 0
+        self.level = 1
+        self.xp = 0
+        self.gold = 0
+        self.inventory = []
+        self.weapon = None
+        self.armor = None
+        self.ring = ring("Platzhalter-Ring", "ring", "Random ass test-ring", "rache", 0  )
+        self.necklace = None
+        self.vitality = player_class['vitality'] + self.ring.ring_vitality
+        self.base_hp = player_class['hp'] 
+        self.max_hp = player_class['hp']
+        self.max_max_hp = self.max_hp * self.vitality
+        self.hp = self.max_max_hp
+        self.strength = player_class['strength']
+        self.attack_damage = player_class['attack_damage']
+
+    def player_attack(self, target, weapon):
+        self.target = target
+        final_damage_dealt = (self.attack_damage + weapon.weapon_damage) * self.strength
+        target.hp -= final_damage_dealt
+        print(f"{self.name} attackiert {target.name} und verursacht {final_damage_dealt} Schaden")
+        print(f"{target.name} hat noch {target.hp} Leben!")
+
+    def pick_up_item(self, item):
+        self.inventory.append(item)
+        print(f"{self.name} hat {item} aufgenommen!")
+
+    def show_inventory(self):
+        print("\nInventar:")
+        if self.inventory:
+            for idx, item in enumerate(self.inventory, start = 1):
+                print(f"{idx}. {item}")
+        else:
+            print("Dein Inventar ist leer")
+
+    def save_game(self):
+        save_data = {
+            'name': self.name,
+            'player_class': self.player_class,
+            'name_of_player_class': self.name_of_player_class,
+            'hp': self.hp, 
+            'max_hp': self.max_hp, 
+            'vitality': self.vitality, 
+            'strength': self.strength, 
+            'attack_damage': self.attack_damage,
+            'skillpoints': self.skillpoints, 
+            'level': self.level, 
+            'xp': self.xp, 
+            'gold': self.gold, 
+            'inventory': [
+            {'type': type(item).__name__, **vars(item)} for item in self.inventory  # Speichere Typ und Attribute
+            ], 
+            'weapon': vars(self.weapon) if self.weapon else None,  # Objekt in ein Dictionary umwandeln
+            'armor': vars(self.armor) if self.armor else None,
+            'ring': vars(self.ring) if self.ring else None,
+            'necklace': vars(self.necklace) if self.necklace else None
+        }
+        with open(f"{self.name}_save.json", "w") as file:
+            json.dump(save_data, file)
+
+    @staticmethod
+    def load_game(file_name):
+        if os.path.exists(file_name):
+            with open(file_name, "r") as file:
+                data = json.load(file)
+                player_class = data['player_class']
+                player = Player(data['name'],player_class)
+                player.name_of_player_class = data['name_of_player_class']
+                player.hp = data['hp']
+                player.max_hp = data['max_hp']
+                player.vitality = data['vitality']
+                player.strength = data['strength']
+                player.attack_damage = data['attack_damage']
+                player.level = data['level']
+                player.xp = data['xp']
+                player.gold = data['gold']
+
+                player.inventory = []
+                for item_data in data['inventory']:
+                    item_type = item_data['type']  # Extrahiere den Typ des Items
+                    
+                    if item_type == 'weapon':
+                        item = weapon(**item_data)
+                    elif item_type == 'armor':
+                        item = armor(**item_data)
+                    elif item_type == 'ring':
+                        item = ring(**item_data)
+                    elif item_type == 'necklace':
+                        item = necklace(**item_data)
+                    elif item_type == 'consumable':
+                        item = consumable(**item_data)
+                    else:
+                        item = item(**item_data)  # Generische Basisklasse
+                    player.inventory.append(item)
+                
+                player.weapon = weapon(**data['weapon']) if data['weapon'] else None  # Objekt aus Dictionary erstellen
+                player.armor = armor(**data['armor']) if data['armor'] else None
+                player.ring = ring(**data['ring']) if data['ring'] else None
+                player.necklace = necklace(**data['necklace']) if data['necklace'] else None
+                print(f"Spielstand von {player.name} wurde erfolgreich geladen!")
+                return player
+        else:
+            print("Speicherstand nicht gefunden!")
+            return None
+    
+    
+        
+        
+CLASSES = {
+    "Elf" : {"name_of_player_class": "Elf", "hp": 110, "vitality": 1, "attack_damage": 18, "strength": 1.3},
+    "Demon" : {"name_of_player_class": "Demon", "hp": 125, "vitality": 1, "attack_damage": 25, "strength": 1.5},
+    "Dwarf" : {"name_of_player_class": "Dwarf", "hp": 150, "vitality": 1.3, "attack_damage": 15, "strength": 1.3},
+    "Orc" : {"name_of_player_class": "Orc", "hp": 120, "vitality": 1, "attack_damage": 23, "strength": 1.5},
+    "Human" : {"name_of_player_class": "Human", "hp": 100, "vitality": 1.5, "attack_damage": 20, "strength": 1},
+    "Inchling" : {"name_of_player_class": "Inchling", "hp": 80, "vitality": 1.5, "attack_damage": 15, "strength": 1.5}
+}
+
+class Enemy:
+    def __init__(self, which_monster):
+        self.name = which_monster[str('name')]
+        self.hp = which_monster['hp']
+        self.max_hp = self.hp
+        self.attack_damage = which_monster['attack_damage']
+
+    def enemy_attack(self, target, weapon):
+        self.target = target
+        final_damage_dealt = (self.attack_damage + weapon.weapon_damage) * (Player.level * 0.7)
+        target.hp -= final_damage_dealt
+        print(f"{self.name} attackiert {target.name} und verursacht {final_damage_dealt} Schaden")
+        print(f"{target.name} hat noch {target.hp} Leben!")
+    
+
+    def __str__(self):
+        return f"{self.name} (HP: {self.hp}, Attack: {self.attack_damage})"
+
+monsters = {
+    "Dragon" : {"name": "Dragon","hp": 220,"attack_damage": 25},
+    "Skeleton" : {"name": "Skeleton","hp": 70,"attack_damage": 18},
+    "Orc" : {"name": "Orc","hp": 170,"attack_damage": 20},
+    "Phantom" : {"name": "Phantom","hp": 130,"attack_damage": 19},
+    "Ghost" : {"name": "Ghost","hp": 120,"attack_damage": 15},
+    "Zombie" : {"name": "Zombie","hp": 135,"attack_damage": 20},
+    "Sheep" : {"name": "Sheep","hp": 30,"attack_damage": 10},
+    "Bandit" : {"name": "Bandit","hp": 140,"attack_damage": 19}
+}
+
+dungeon_monsters = {
+    "Mimic" : {"name": "Mimic", "hp": 180, "attack_damage": 30},
+}
+
+events = [
+    "event_enemy_encounter",
+    "event_wandering_trader_encounter",
+    "event_player_finds_village",
+    "event_player_finds_dungeon",
+    "event_player_finds_random_house",
+    "event_npc_encounter"
+    "event_player_finds_ruins",
+    "event_payer_finds_ancient_ruins",
+    "event_player_finds_camp",
+    "event_player_finds_hostile_camp",
+    "event_player_random_thoughts",
+    "event_bird_shits_on_player",
+    "event_random_animal",
+]
+
+items = {
+    "Dolch": {"name": "Dolch", "type": "weapon", "item_info": "Ein kleiner Dolch, der gut in deine Tasche passt.", "weapon_type": "dolch", "weapon_damage": 4},
+    "Kelch": {"name": "Kelch", "type": "item", "item_info": "Ein mysteriöser, goldener Kelch, den du gefunden hast."},
+    "Ring des Abyss": {"name": "Ring des Abyss", "type": "ring", "item_info": "Dieser Ring strahlt eine Atmosphäre aus,\ndie alles um sich herum aufzusaugen scheint.. \ngruselig...", "ring_type": "abyss", "ring_vitality": 1.2},
+}
+
+items_from_chests = {
+    "Dolch": {"name": "Dolch", "type": "weapon", "item_info": "Ein kleiner Dolch, der gut in deine Tasche passt.", "weapon_type": "dolch", "weapon_damage": 4},
+    "Goldkelch": {"name": "Kelch", "type": "item", "item_info": "Ein mysteriöser, goldener Kelch, den du gefunden hast."},
+    "Ring des Abyss": {"name": "Ring des Abyss", "type": "ring", "item_info": "Dieser Ring strahlt eine Atmosphäre aus,\ndie alles um sich herum aufzusaugen scheint.. \ngruselig...", "ring_type": "abyss", "ring_vitality": 1.2},
+    "Scholle": {"name": "Scholle", "type": "weapon", "item_info": "Riecht ranzig, scheint aber niemals vergammeln zu können.", "weapon_type": "fisch", "weapon_damage": 15},
+    "Großschwert": {"name": "Großschwert", "type": "weapon", "item_info": "Ein badass großes Schwert, dass dich befähigt,\ndeinen Feinden so richtig in den Hintern zu treten.", "weapon_type": "großschwert", "weapon_damage": 25},
+    
+}
+
+def get_random_monster(monsters):
+    which_monster = random.choice(list(monsters.values()))
+    return which_monster
+
+def get_random_item(items):
+    which_item = random.choice(list(items.values()))
+    return which_item
+
+class item:
+    def __init__(self, name, type, item_info):
+        self.name = name
+        self.type = type
+        self.item_info = str(item_info)
+
+    def __str__(self):
+        return f"{self.name} ({self.type}): \n{self.item_info}"
+    
+class weapon(item):
+    def __init__(self, name, type, item_info, weapon_type, weapon_damage):
+        super().__init__(name, type, item_info)
+        self.weapon_type = weapon_type
+        self.weapon_damage = weapon_damage
+
+    def __str__(self):
+        return f"{self.name} ({self.type}): {self.weapon_damage} Damage\n{self.item_info}"
+
+class armor(item):
+    def __init__(self, name, type, item_info, armor_type, armor_vitality):
+        super().__init__(name, type, item_info)
+        self.armor_type = armor_type
+        self.armor_vitality = armor_vitality
+
+class ring(item):
+    def __init__(self, name, type, item_info, ring_type, ring_vitality):
+        super().__init__(name, type, item_info)
+        self.ring_type = ring_type
+        self.ring_vitality = ring_vitality
+
+    def __str__(self):
+        return f"{self.name} ({self.type}): {self.ring_vitality} Vitality\n{self.item_info}"
+
+class necklace(item):
+    def __init__(self, name, type, item_info, necklace_type, necklace_strength):
+        super().__init__(name, type, item_info)
+        self.armor_type = necklace_type
+        self.armor_vitality = necklace_strength
+
+class consumable(item):
+    def __init__(self, name, type, item_info, consumable_type, hp_player_gets):
+        super().__init__(name, type, item_info)
+        self.consumable_type = consumable_type
+        self.hp_player_gets = int(hp_player_gets)
