02dc01e025e68a8a8fd5bc8de01bfd4ba9d87385
InkyyPinkyy <inkyy.social@gmail.com>
2025-05-08 19:29:59 +0200

    Moved dungeon_sizes from classes.py to dungeon.py

--- classes.py
+++ classes.py
@@ -404,35 +404,3 @@ class consumable(item):
 
     def __str__(self):
         return f"{self.name} ({self.type}): {self.stats_player_gets} HP\n{self.item_info}"
-    
-dungeon_sizes = {
-    "small": [ # 15 rooms
-        ["A1", "A2", "A3"],
-        ["B1", "B2", "B3"],
-        ["C1", "C2", "C3"],
-        ["D1", "D2", "D3"],
-        ["E1", "E2", "E3"]
-        ],
-
-    "medium": [ # 28 rooms
-        ["A1", "A2", "A3", "A4"],
-        ["B1", "B2", "B3", "B4"],
-        ["C1", "C2", "C3", "C4"],
-        ["D1", "D2", "D3", "D4"],
-        ["E1", "E2", "E3", "E4"],
-        ["F1", "F2", "F3", "F4"],
-        ["G1", "G2", "G3", "G4"]
-        ],
-
-    "large": [ # 45 rooms
-        ["A1", "A2", "A3", "A4", "A5"],
-        ["B1", "B2", "B3", "B4", "B5"],
-        ["C1", "C2", "C3", "C4", "C5"],
-        ["D1", "D2", "D3", "D4", "D5"],
-        ["E1", "E2", "E3", "E4", "E5"],
-        ["F1", "F2", "F3", "F4", "F5"],
-        ["G1", "G2", "G3", "G4", "G5"],
-        ["H1", "H2", "H3", "H4", "H5"],
-        ["I1", "I2", "I3", "I4", "I5"]
-        ],
-}
